9321dae366d120b07a13c792ef2ab85d35e60d83
Artur <rtrnonato@gmail.com>
2025-01-15 18:57:58 -0300

    Initial commit

---
+++ Functional programming/stream-exercise/src/main/java/com/rtrnonato/menuonline/StreamExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.rtrnonato.menuonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamExerciseApplication.class, args);
+	}
+
+}
