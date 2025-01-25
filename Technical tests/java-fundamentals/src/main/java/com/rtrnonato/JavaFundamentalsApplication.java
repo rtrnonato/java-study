5a148a7f33c69dfd7368f5348ffadfac8825c1b6
Artur <rtrnonato@gmail.com>
2025-01-25 13:30:07 -0300

    Starting technical tests

---
+++ Technical tests/java-fundamentals/src/main/java/com/rtrnonato/JavaFundamentalsApplication.java
@@ -0,0 +1,31 @@
+package com.rtrnonato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFundamentalsApplication.class, args);
+
+		/*
+		 * Questão-1 
+		 * Escreva um método em Java chamado isPalindrome que receba uma
+		 * string como entrada e retorne true se a string for um palíndromo (ou seja,
+		 * lida da mesma forma de trás para frente). Ignore espaços e diferenças entre
+		 * maiúsculas e minúsculas.
+		 * 
+		 * public boolean isPalindrome(String input) { 
+		 *    TODO:Implementar 
+		 * } 
+		 * 
+		 * Questão-2 
+		 * Explique, com um exemplo de código, a diferença entre
+		 * os modificadores de acesso public, private, protected e o default (pacote).
+		 * 
+		 */
+
+	}
+
+}
---
+++ Technical tests/java-fundamentals/src/test/java/com/rtrnonato/JavaFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtrnonato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
