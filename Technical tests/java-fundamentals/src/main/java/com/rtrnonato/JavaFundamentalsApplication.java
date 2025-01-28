a8ee399e01637d8e0e1e6c38a0be1c7d5fa952ca
Artur <rtrnonato@gmail.com>
2025-01-28 16:29:05 -0300

    Java Fundamentals completed

--- Technical tests/java-fundamentals/src/main/java/com/rtrnonato/JavaFundamentalsApplication.java
+++ Technical tests/java-fundamentals/src/main/java/com/rtrnonato/JavaFundamentalsApplication.java
@@ -1,5 +1,8 @@
 package com.rtrnonato;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,19 +16,40 @@ public class JavaFundamentalsApplication {
 		 * Questão-1 
 		 * Escreva um método em Java chamado isPalindrome que receba uma
 		 * string como entrada e retorne true se a string for um palíndromo (ou seja,
-		 * lida da mesma forma de trás para frente). Ignore espaços e diferenças entre
+		 * lida da mesma forma de trás pa
+		 * a frente). Ignore espaços e diferenças entre
 		 * maiúsculas e minúsculas.
 		 * 
 		 * public boolean isPalindrome(String input) { 
 		 *    TODO:Implementar 
 		 * } 
 		 * 
-		 * Questão-2 
-		 * Explique, com um exemplo de código, a diferença entre
-		 * os modificadores de acesso public, private, protected e o default (pacote).
-		 * 
 		 */
-
+		
+		System.out.println(isPalindrome("A man a plan a canal Panama")); // true
+		System.out.println(isPalindrome("racecar")); // true
+		System.out.println(isPalindrome("hello")); // false
+		System.out.println(isPalindrome("Was it a car or a cat I saw")); // true
+		System.out.println(isPalindrome("12321")); // true
+		System.out.println(isPalindrome("12345")); // false
+		}
+	
+	public static boolean isPalindrome(String input) {
+		// Limpar a entrada: remover espaços e converter para letras minúsculas.
+		String sanitized = input.replaceAll("\\s+","").toLowerCase();
+		
+		// Verificar se a string é igual ao seu reverso
+		int left = 0;
+	    int right = sanitized.length() - 1;
+		
+	    while (left < right) {
+			if (sanitized.charAt(left) != sanitized.charAt(right)) {
+				return false;
+			}
+			left++;
+            right--;
+		}
+		
+	    return true;
 	}
-
 }
